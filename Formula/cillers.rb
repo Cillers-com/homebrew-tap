0c15c7f86f723df43916c5b83fcc177c7a057117
Peder Linder <pederbl@gmail.com>
2024-05-04 11:44:49 +0200

    Update formula to v0.0.2

--- Formula/cillers.rb
+++ Formula/cillers.rb
@@ -1,8 +1,8 @@
 class Cillers < Formula
   desc "The Cillers CLI"
   homepage "https://cillers.com"
-  version "0.0.1"
-  url "https://github.com/Cillers-com/cillers-cli/archive/refs/tags/v0.0.1.tar.gz"
+  version "0.0.2"
+  url "https://github.com/Cillers-com/cillers-cli/archive/refs/tags/v0.0.2.tar.gz"
   sha256 "8b3dadf444263f88f78c850197f0f19a8bf49bbcec58518eb713d7b933a22d67"
 
   def install
