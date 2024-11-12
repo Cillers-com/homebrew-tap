704977a1bcf7324a28a361c19025bf3e7011785c
Cillers CLI Build Script <noreply@cillers.com>
2024-11-12 12:38:48 +0100

    Update Cillers CLI to version v0.2.5

--- Formula/cillers.rb
+++ Formula/cillers.rb
@@ -1,16 +1,16 @@
 class Cillers < Formula
   desc "The Cillers CLI"
   homepage "https://cillers.com"
-  version "v0.2.4"
+  version "v0.2.5"
 
   @@os = "#{OS.mac? ? 'macos' : 'linux'}"
   @@arch = "#{Hardware::CPU.intel? ? 'amd64' : 'arm64'}"
   @@image_variant = "#{@@os}-#{@@arch}"
   @@shasums = {
-    "linux-amd64" => "7908a7fad423b948f7fa5138d901347fb8155ce790812bffd516613137a8201f",
-    "linux-arm64" => "ac011f02ddd0019a7f2b143fea4fe9f734869720fa3ebe934437609bb90f3fe7",
-    "macos-amd64" => "eb8e6030e1c32b2e6343769bfab4ed6b66b2414e60df87e64ce619fb6970ca1e", 
-    "macos-arm64" => "98fab914900e325a0f3f64bb37e780db00c760d260e94c8527337751cd9d2c79"
+    "linux-amd64" => "21ff3442ded908d2cd842145e06c34daacf617543fcaf5606b3a638e9ff38dff",
+    "linux-arm64" => "3891edceb6305f5ee58920e384f314c83ea349eec6cdc4a6c2daad4e3273c5e7",
+    "macos-amd64" => "673abce953fa6b5366bc66386ad0cd1178b2d29313edcff69e625dbf7b7d8ba4", 
+    "macos-arm64" => "19859003ee6bea0413c50755a7977f1dd516ac14c69cfde46deedea1c5696406"
   }
   
   url "https://storage.googleapis.com/cillers-cli/cillers-cli-#{version}-#{@@image_variant}.tar.gz"
